8e81da274bbabb672da3c23bca922a70dacf151e
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-09-05 21:16:32 -0300

    dados compostos II

---
+++ dadoscompostosII.py
@@ -0,0 +1,10 @@
+# Exemplo com dicionarios/objeto
+pessoa = {"Nome": "Ana", "idade": 30}
+print("Pessoa:", pessoa)
+
+#Acessando valores
+print("Nome:", pessoa["Nome"])
+
+# Adicionando um novo par chave-valor
+pessoa["cidade"] = "São Paulo"
+print("Pessoa atualizada:", pessoa)
