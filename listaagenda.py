6b41f03bf3b43b8760a9817602ec246f8b01d8c6
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-09-05 21:32:43 -0300

    lista agenda

---
+++ listaagenda.py
@@ -0,0 +1,16 @@
+listaPessoas = []
+
+pessoa = {"nome": "Ana", "idade": 32}
+listaPessoas.append(pessoa)
+print(listaPessoas)
+
+pessoa = {"nome": "Brenda", "idade": 26}
+listaPessoas.append(pessoa)
+print(listaPessoas)
+
+pessoa = {"nome": "Fernanda", "idade": 48}
+listaPessoas.append(pessoa)
+print(listaPessoas)
+
+Brenda = listaPessoas[1]
+print(Brenda)
