e342405c0870034d1604582373b3db7c75c423a4
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-09-05 21:05:38 -0300

    dados compostos

---
+++ dadoscompostos.py
@@ -0,0 +1,7 @@
+frutas = ["maça", "banana", "laranja"]
+print("Lista de frutas:", frutas)
+
+frutas.append("uva")
+print("Lista de frutas atualizada:", frutas)
+
+print("Primeira fruta:", frutas [3])
